577940bb09a38fe49aa3daab7a07e8cb968ddeaa
Frederick Solt <fsolt@mac.com>
2016-12-11 20:56:12 -0600

    combine samples when same item is asked in same year

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,12 +1,15 @@
 library(tidyverse)
 
-ukdata <- read_tsv("dev/McGann2014/ukdata_3.txt") %>% 
+ukdata <- read_tsv("dev/McGann2014/ukdata_3.txt") %>%
+    group_by(year, variable, topic) %>% 
+        summarize(y_r = sum(round(leftp/100 * n)), # When two surveys ask the same question in
+                  n = sum(n)) %>% # the same year, add samples together
+    ungroup() %>% 
     mutate(cutpoint = 1,
            variable_cp = paste(variable, cutpoint, sep="_gt"),
            ccode = 1,
            tcode = as.integer(year - min(year) + 1),
            qcode = as.numeric(factor(variable, levels = unique(variable))),
            rcode = as.numeric(factor(variable_cp, levels = unique(variable_cp))),
-           y_r = round(leftp/100 * n),
            ktcode = (ccode-1)*max(tcode)+tcode) %>%
     arrange(ccode, tcode, qcode, rcode)
