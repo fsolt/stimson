4f45dfdc80b1cfc4e2e29065dd1836091b7087d8
Frederick Solt <fsolt@mac.com>
2016-12-13 12:54:11 -0600

    use (aggregate) ordinal data from Bartle2016

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,19 +1,38 @@
 library(tidyverse)
+library(readxl)
+library(stringr)
 
-# This should be revised to make use of the ordinal data available in Bartle2016:
-# "dev/Bartle2016/UK_Preferences_2012.xlsx"
-# https://discover.ukdataservice.ac.uk/catalogue/?sn=852280
+# Bartle2016: https://discover.ukdataservice.ac.uk/catalogue/?sn=852280
 
-ukdata <- read_tsv("dev/McGann2014/ukdata_3.txt") %>%
-    group_by(year, variable, topic) %>% 
-        summarize(y_r = sum(round(leftp/100 * n)), # When two surveys ask the same question in
+ukmp <- read_excel("dev/Bartle2016/UK_Preferences_2012.xlsx") %>% 
+    setNames(tolower(names(.))) %>% 
+    select(variable:n) %>% 
+    gather(key = cp, 
+           value = y, 
+           `0`:`10`,
+           na.rm = TRUE) %>% 
+    mutate(cp = if_else(cp=="neutral", 5, as.numeric(cp)),
+           cp = if_else(polar==1, 10-cp, cp)) %>% # make left always high
+    group_by(house, date, variable) %>% 
+        mutate(cutpoint = as.numeric(factor(cp)) - 1) %>% 
+        arrange(-cutpoint) %>% 
+        mutate(y_r = cumsum(y),
+               variable_cp = paste(variable, cutpoint, sep="_gt"),
+               year = as.numeric(str_extract(date, "\\d{4}")),
+               t = last(y_r)) %>%
+        mutate(y_r = ifelse(t <= 100, y_r*10, y_r),
+               n = ifelse(t <= 100, t*10, t)) %>%  # assume 1000 resp surveyed if y as %
+    ungroup() %>%
+    filter(cutpoint!=0) %>% 
+    group_by(year, variable_cp, variable, topic) %>% 
+        summarize(y_r = sum(y_r), # When two surveys ask the same question in
                   n = sum(n)) %>% # the same year, add samples together
     ungroup() %>% 
-    mutate(cutpoint = 1,
-           variable_cp = paste(variable, cutpoint, sep="_gt"),
-           ccode = 1,
+    mutate(ccode = 1,
            tcode = as.integer(year - min(year) + 1),
            qcode = as.numeric(factor(variable, levels = unique(variable))),
            rcode = as.numeric(factor(variable_cp, levels = unique(variable_cp))),
            ktcode = (ccode-1)*max(tcode)+tcode) %>%
-    arrange(ccode, tcode, qcode, rcode)
+    arrange(ccode, tcode, qcode, rcode)   
+    
+    
